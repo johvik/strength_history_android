7e9922fdf8d8864aeaf8268d60a5fbc804baeaad
Mazo <john.viklund@gmail.com>
2013-05-16 18:07:02 +0200

    Files for new activity

---
+++ src/strength/history/ExercisesActivity.java
@@ -0,0 +1,22 @@
+package strength.history;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class ExercisesActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_exercises);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.exercises, menu);
+		return true;
+	}
+
+}
